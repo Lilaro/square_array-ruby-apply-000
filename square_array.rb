c8db394bc3db57092386cd6b30f0f5276fcceff3
Lisa LaRochelle <larochelle.lisa@gmail.com>
2019-06-13 03:07:05 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,29 @@
+
 def square_array(array)
-  # your code here
+  puts("1. Just entered the function square_array")
+  
+  new_array = []
+  
+  array.each do |number|
+    puts("- In the each statement")
+    puts("- Number: #{number}")
+    squared_number = number ** 2
+    puts("- Squared Number: #{squared_number}")
+     new_array << squared_number
+  end
+  
+  return new_array
+  
 end
+
+originalNubmers = [1, 2, 3, 4, 5]
+
+
+
+squaredNumbers = square_array(originalNubmers)
+
+
+puts(squaredNumbers)
+
+
+
